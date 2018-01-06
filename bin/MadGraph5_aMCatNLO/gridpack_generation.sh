da5afb224243e08b8ec2656f4dd7f9c76287a249
Raffaele <raffaele.gerosa@cern.ch>
2018-01-06 11:42:09 +0100

    updates

--- bin/MadGraph5_aMCatNLO/gridpack_generation.sh
+++ bin/MadGraph5_aMCatNLO/gridpack_generation.sh
@@ -538,16 +538,16 @@ if [ -e $CARDSDIR/${name}_param_card.dat ]; then
 fi
 
 #copy provided custom fks params or cuts
-if [ -e $CARDSDIR/${name}_cuts.f ]; then
-  echo "copying custom cuts.f file"
-  cp $CARDSDIR/${name}_cuts.f ./SubProcesses/cuts.f
-fi
+#if [ -e $CARDSDIR/${name}_cuts.f ]; then
+#  echo "copying custom cuts.f file"
+#  cp $CARDSDIR/${name}_cuts.f ./SubProcesses/cuts.f
+#fi
 
 #copy provided custom fks params or cuts
-if [ -e $CARDSDIR/${name}_setcuts.f ]; then
-  echo "copying custom setcuts.f file"
-  cp $CARDSDIR/${name}_setcuts.f ./SubProcesses/setcuts.f
-fi
+#if [ -e $CARDSDIR/${name}_setcuts.f ]; then
+#  echo "copying custom setcuts.f file"
+#  cp $CARDSDIR/${name}_setcuts.f ./SubProcesses/setcuts.f
+#fi
 
 if [ -e $CARDSDIR/${name}_FKS_params.dat ]; then
   echo "copying custom FKS_params.dat file"
